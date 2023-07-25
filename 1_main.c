367e28ea93d04023dd5884ac7af84ff76eed96db
M-10001 <67879252+M-10001@users.noreply.github.com>
2023-07-25 10:21:04 +0100

    Move from GTK4 to GTK3

--- 1_main.c
+++ 1_main.c
@@ -2,11 +2,13 @@
 #include <gtk/gtk.h>
 
 static void activate (GtkApplication* app, gpointer user_data){
-  GtkWidget *window_1;
+  GtkWidget * window_1;
+  GtkWidget * stage;
 
   window_1 = gtk_application_window_new (app);
   gtk_window_set_title(GTK_WINDOW(window_1), "Crosses and circles");
   gtk_window_set_default_size(GTK_WINDOW(window_1), 400, 400);
+
   gtk_window_present(GTK_WINDOW(window_1));
 }
 
